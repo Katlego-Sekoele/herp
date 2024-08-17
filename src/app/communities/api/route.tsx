d346c6f190762a48d53dfd7b4c68d1c53654dbbe
Katlego-Sekoele <sekoelekatlego@gmail.com>
2024-08-17 20:28:10 +0200

    Delete unused API route for communities

--- src/app/communities/api/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	// Your code here
-
-	// Example: Return a JSON response
-	res.status(200).json({ message: "Hello from the API route!" });
-}
